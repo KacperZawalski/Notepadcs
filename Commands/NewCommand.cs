f2776ea04b87424ccd375debcbc752ad6bcfb260
Kacper Zawalski <kacperzawalski3@gmail.com>
2023-07-21 10:46:24 +0200

    Added NewFile Command

---
+++ Commands/NewCommand.cs
@@ -0,0 +1,26 @@
+﻿using Notepadcs.Models;
+using Notepadcs.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notepadcs.Commands
+{
+    public class NewCommand : CommandBase
+    {
+        private NotepadM _notepadM;
+        private NotepadViewModel _notepadViewModel;
+        public NewCommand(NotepadM notepadM, NotepadViewModel notepadViewModel)
+        {
+            _notepadM = notepadM;
+            _notepadViewModel = notepadViewModel;
+        }
+        public override void Execute(object? parameter)
+        {
+            _notepadM.fileLogic.ResetFilePath();
+            _notepadM.Text = _notepadViewModel.Text = "";
+        }
+    }
+}
---
+++ Commands/OpenCommand.cs
@@ -0,0 +1,26 @@
+﻿using Notepadcs.Models;
+using Notepadcs.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notepadcs.Commands
+{
+    public class OpenCommand : CommandBase
+    {
+        private NotepadM _notepadM;
+        private NotepadViewModel _notepadViewModel;
+        public OpenCommand(NotepadM notepadM, NotepadViewModel notepadViewModel)
+        {
+            _notepadM = notepadM;
+            _notepadViewModel = notepadViewModel;
+        }
+        public override void Execute(object? parameter)
+        {
+            _notepadViewModel.Text = _notepadM.Text = _notepadM.fileLogic.OpenFile();
+            
+        }
+    }
+}
