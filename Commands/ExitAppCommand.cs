053aa98eb863c1608e48f7a8c04966ce1cdb9a54
Kacper Zawalski <kacperzawalski3@gmail.com>
2023-07-25 10:28:45 +0200

    Added exit menuitem in file tab

---
+++ Commands/ExitAppCommand.cs
@@ -0,0 +1,22 @@
+﻿using Notepad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+
+
+namespace Notepadcs.Commands
+{
+    public class ExitAppCommand : CommandBase
+    {
+        public override void Execute(object? parameter)
+        {
+            Application.Current.MainWindow.Close();
+        }
+    }
+}
